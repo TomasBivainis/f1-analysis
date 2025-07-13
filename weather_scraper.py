6bf558635c011fdaf99844a8ce1b2ba2ece4fae1
Tomas Bivainis <bivainis.tomas@gmail.com>
2025-07-13 15:25:01 +0300

    started the weather scraper

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,11 +1,18 @@
 from json_util import *
+from race_util import Race, Racetrack
 
 def get_race_weather():
-  racetracks = load_json("racetracks")
-  race_results = load_json("race_results")
+  racetracks = convert_dict_list_to_object_list(load_json("racetracks.json"), Racetrack)
+  race_results = convert_dict_list_to_object_list(load_json("race_results.json"), Race)
   
-  print(racetracks)
-  print(race_results)
+  completed_race_coordinates = {}
+  
+  for race in race_results:
+    for racetrack in racetracks:
+      if race.city == racetrack.city:
+        completed_race_coordinates[race.city] = [racetrack.latitude, racetrack.longitude, race.date_end]
+  
+  print(completed_race_coordinates)
 
 
 if __name__ == "__main__":
