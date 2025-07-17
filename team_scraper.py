8cb2c715dbdcb5424196d97f1acfc4ef5f2a501e
Tomas Bivainis <bivainis.tomas@gmail.com>
2025-07-17 19:32:46 +0300

    get teams for json data

---
+++ team_scraper.py
@@ -0,0 +1,14 @@
+from json_util import *
+
+def get_all_teams():
+  race_result = load_json("race_results.json")[0]["race_result"]
+  
+  teams = []
+  
+  for driver in race_result:
+    if driver["team"] not in teams:
+      teams.append(driver["team"])
+  
+  return teams
+  
+  
